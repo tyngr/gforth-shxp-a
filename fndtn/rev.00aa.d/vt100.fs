2f0e1510b5212e2b007c466a9d72d0cf4eb87c6c
tyngr <tyngr@users.noreply.github.com>
2019-10-08 08:10:31 +0000

    esc[[ with brief doco

--- fndtn/rev.00aa.d/vt100.fs
+++ fndtn/rev.00aa.d/vt100.fs
@@ -1,5 +1,16 @@
+: esc[[ 27 [char] [ swap emit emit ; \ system declared esc[ already!
 : go cr 10 spaces 5 0 do 65 emit loop cr cr ;
 
 0 [IF]
   Anything inside this construct is disregarded by gforth
+[THEN]
+
+0 [IF]
+
+esc[[ ( -- )
+  emits ESC[  (ascii 27, and left square brace)
+  as preamble to most vt100 escape sequences to do
+  things with the terminal such as reposition the
+  cursor, or draw in a different color (or intensity).
+
 [THEN]
