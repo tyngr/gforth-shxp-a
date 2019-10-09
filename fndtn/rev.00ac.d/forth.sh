2c3807c2993a33a1d18704d7d9c79150e509f628
tyngr <tyngr@users.noreply.github.com>
2019-10-09 08:15:11 +0000

    forth.sh

--- fndtn/rev.00ac.d/forth.sh
+++ fndtn/rev.00ac.d/forth.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-/usr/bin/gforth job.fs -e "${1} ${2} ${3} ${4} ${5} ${6} ${7} ${8} ${9} bye"
+# /usr/bin/gforth job.fs -e "${1} ${2} ${3} ${4} ${5} ${6} ${7} ${8} ${9} bye"
+/usr/bin/gforth job.fs -e "$* bye"
